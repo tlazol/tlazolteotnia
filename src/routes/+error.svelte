9d67ed6dfbcf4896707e4b1fe0d305b7a7b80d1e
小林大介 <stinkysecure@gmail.com>
2023-03-16 15:26:48 +0900

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+</script>
+
+<h1>{$page.status}</h1>
+
+<style>
+  h1 {
+    text-align: center;
+    font-size: 8rem;
+    margin-top: 2rem;
+  }
+</style>
