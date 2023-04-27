f37c7fd49f7b10df34c350a6ec32ad49a8bf76be
小林大介 <stinkysecure@gmail.com>
2023-04-27 17:29:27 +0900

    pdf

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -54,18 +54,6 @@
       <NendAd />
     </div>
     {@html data.blogEntry.fields.markdown}
-    {#if data.blogEntry.fields.pdf}
-      <iframe
-        class="pdf"
-        title={`${data.blogEntry.fields.text}`}
-        src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://0rga.org/pdf/{data
-          .blogEntry.sys.id}.pdf"
-        scrolling="no"
-        marginwidth="0"
-        marginheight="0"
-        frameborder="0"
-      />
-    {/if}
     <div class="dmmAdWrap">
       <DmmAd />
     </div>
@@ -117,12 +105,6 @@
 
   .dmmAdWrap {
     margin-top: 3rem;
-  }
-
-  .pdf {
-    width: 100%;
-    aspect-ratio: 16 / 9;
-    border: none;
   }
 
   @media (min-width: 480px) {
